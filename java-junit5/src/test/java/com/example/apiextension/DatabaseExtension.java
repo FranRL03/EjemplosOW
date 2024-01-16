4cced7089570531986b63e241b52b676371f7125
Fran Ruiz <fjavierrule@gmail.com>
2024-01-16 20:52:04 +0100

    testing avanzado

---
+++ java-junit5/src/test/java/com/example/apiextension/DatabaseExtension.java
@@ -0,0 +1,34 @@
+package com.example.apiextension;
+
+import com.example.demo.extensions.ConnectionDB;
+import com.example.demo.extensions.DirectionDDL;
+import org.junit.jupiter.api.extension.*;
+
+import java.sql.Connection;
+
+public class DatabaseExtension implements BeforeAllCallback, BeforeEachCallback, AfterAllCallback {
+
+    private Connection connection;
+
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        connection = ConnectionDB.openConnection();
+    }
+
+    /* cada test va a partir con una base de datos de estado limpio */
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        DirectionDDL.dropTable(connection);
+        DirectionDDL.createTable(connection);
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        ConnectionDB.closeConnection();
+    }
+
+
+
+
+}
--- spring-data-jpa-criteria/src/main/java/com/jpa/criteria/springdatajpacriteria/repository/ProductDAO.java
+++ spring-data-jpa-criteria/src/main/java/com/jpa/criteria/springdatajpacriteria/repository/ProductDAO.java
@@ -71,7 +71,7 @@ public class ProductDAO {
         CriteriaQuery<Product> findByPredicates =  criteria
                 .select(product)
                 .where(finalPred);
-        
+
         return entityManager.createQuery(findByPredicates).getResultList();
     }
 }
